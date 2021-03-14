4df3d2378cfc2292a0afba7f288d6d364f3ea1be
LeonGr <leongrasmeijer@gmail.com>
2021-03-14 12:13:07 +0100

    Expand zsh config

--- .config/nixpkgs/modules/zsh.nix
+++ .config/nixpkgs/modules/zsh.nix
@@ -7,6 +7,34 @@
         enableCompletion = true;
         enableAutosuggestions = true;
 
+        shellAliases = {
+            phpserver = "php -S localhost:8000";
+            pythonserver = "python -m http.server";
+            py = "python";
+            ":wq" = "exit";
+            ":q" = "exit";
+            music = "echo 'Song: ' && playerctl metadata 'xesam:title' && echo '\nAlbum: ' && playerctl metadata 'xesam:album' && echo '\nArtist: ' && playerctl metadata 'xesam:albumArtist'";
+            exuent = "exit";
+            spotify = "spotify --force-device-scale-factor = 1.5";
+            status = "sudo systemctl status";
+            stop = "sudo systemctl stop";
+            start = "sudo systemctl start";
+            restart = "sudo systemctl restart";
+            sus = "systemctl suspend";
+            tmux = "TERM = xterm-256color tmux"; # make cursor work
+            mv = "mv -i"; # (--interactive) confirm overwrites
+
+            # ls -> exa
+            exa = "exa --git";
+            ls = "exa";
+            ll = "exa -l";
+            la = "exa -la";
+            lt = "exa -T";
+            lr = "exa -R";
+            lat = "exa -laT";
+            lar = "exa -laR";
+        };
+
         history = {
             path = "${config.xdg.dataHome}/zsh/zsh_history";
             save = 50000;
@@ -19,12 +47,16 @@
 
         initExtra = ''
             #source /home/leon/dotfiles/zsh/.zshrc
-            export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh
+            #export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh
 
-            ZSH_THEME="alanpeabody"
-
-            plugins=(git zsh zsh-autosuggestions zsh-syntax-highlighting z)
+            #plugins=(git zsh zsh-autosuggestions zsh-syntax-highlighting z)
         '';
+
+        oh-my-zsh = {
+            enable = true;
+            theme = "alanpeabody";
+            plugins = [ "git" "z" ];
+        };
 
         plugins = [
             rec {
